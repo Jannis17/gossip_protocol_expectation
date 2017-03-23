4a96f10ef4305e4ac68c122ed27b7eaed8174129
Jannis17 <ykokkinis@gmail.com>
2017-03-23 17:35:42 +0100

    removed LNS

--- LNS/include/gauss.h
+++
@@ -1,6 +0,0 @@
-//~ #ifndef H_GAUSS_H_
-//~ #define H_GAUSS_H_
-
-//~ float gaussElimination (struct queue_t*, int);
-
-//~ #endif
--- LNS/include/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_MAIN_H_
-#define H_MAIN_H_
-
-/* MAXN = ULTIMATELY MAXIMUM number of agents */
-#define MAXN 10
-
-#endif /* H_MAIN_H_ */
